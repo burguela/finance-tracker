cdfeee2cfe6eed1b2bb18bbcb63072ae804641ad
João Luiz <joaoluizleiteteixeira@gmail.com>
2024-06-08 17:59:41 -0300

    projeto quase final

---
+++ composables/useRedirectIfAuthenticated.js
@@ -0,0 +1,15 @@
+const useRedirectIfAuthenticated = (url = "/") => {
+  const user = useSupabaseUser();
+
+  watch(
+    user,
+    (user) => {
+      if (user) {
+        return navigateTo(url);
+      }
+    },
+    { immediate: true }
+  );
+};
+
+export default useRedirectIfAuthenticated;
